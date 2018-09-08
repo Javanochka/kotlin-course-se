818c6f9b027a64b56d9b22ac491dc6f68bd45e03
Anna Nikiforovskaya <nikiannanik@gmail.com>
2018-09-09 01:30:00 +0300

    HW 1 done: solved 1027D on Kotlin

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,71 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+class Input(val numberOfRooms: Int, val cost: IntArray, val nextRoom: IntArray)
+
+fun readInput(scanner: Scanner): Input {
+    val numberOfRooms = scanner.nextInt()
+    val cost = IntArray(numberOfRooms, {scanner.nextInt()})
+    val nextRoom = IntArray(numberOfRooms, {scanner.nextInt() - 1})
+    return Input(numberOfRooms, cost, nextRoom)
+}
+
+class Solution(private val input: Input) {
+
+    private enum class Color {
+        WHITE, GREY, BLACK
+    }
+
+    private val colors = Array(input.numberOfRooms, { Color.WHITE })
+    private val stack = mutableListOf<Int>()
+
+    private fun calculateCostOfReachableTraps(currentRoom: Int): Int {
+        colors[currentRoom] = Color.GREY
+        stack.add(currentRoom)
+        val followingRoom = input.nextRoom[currentRoom]
+        val answer = when (colors[followingRoom]) {
+            Color.GREY -> {
+                var i = stack.size - 1
+
+                var bestTrap = followingRoom
+                while (stack[i] != followingRoom) {
+                    if (input.cost[bestTrap] > input.cost[stack[i]]) {
+                        bestTrap = stack[i]
+                    }
+                    i--
+                }
+                input.cost[bestTrap]
+            }
+            Color.WHITE -> calculateCostOfReachableTraps(followingRoom)
+            Color.BLACK -> 0
+        }
+        colors[currentRoom] = Color.BLACK
+        stack.removeAt(stack.size - 1)
+        return answer
+    }
+
+    private var gotAnswer = false
+
+    fun getAnswer(): Int {
+        if (gotAnswer) {
+            throw GetAnswerCalledAgainException()
+        }
+        gotAnswer = true
+        var answer = 0
+
+        for (i in 0 until input.numberOfRooms) {
+            if (colors[i] == Color.WHITE) {
+                answer += calculateCostOfReachableTraps(i)
+            }
+        }
+        return answer
+    }
+
+    class GetAnswerCalledAgainException : Exception("Function getAnswer should be called once")
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val input = readInput(Scanner(System.`in`))
+    println(Solution(input).getAnswer())
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,75 @@
 package ru.hse.spb
 
+import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testReadInputSample1() {
+        val sample = "5\n" +
+                "1 2 3 2 10\n" +
+                "1 3 4 3 3"
+        val input = readInput(Scanner(sample))
+        assertEquals(5, input.numberOfRooms)
+        assertArrayEquals(intArrayOf(1, 2, 3, 2, 10), input.cost)
+        assertArrayEquals(intArrayOf(0, 2, 3, 2, 2), input.nextRoom)
+    }
+
+    @Test
+    fun testReadInputSample2() {
+        val sample = "4\n" +
+                "1 10 2 10\n" +
+                "2 4 2 2\n"
+        val input = readInput(Scanner(sample))
+        assertEquals(4, input.numberOfRooms)
+        assertArrayEquals(intArrayOf(1, 10, 2, 10), input.cost)
+        assertArrayEquals(intArrayOf(1, 3, 1, 1), input.nextRoom)
+    }
+
+    @Test
+    fun testReadInputSample3() {
+        val sample = "7\n" +
+                "1 1 1 1 1 1 1\n" +
+                "2 2 2 3 6 7 6"
+        val input = readInput(Scanner(sample))
+        assertEquals(7, input.numberOfRooms)
+        assertArrayEquals(intArrayOf(1, 1, 1, 1, 1, 1, 1), input.cost)
+        assertArrayEquals(intArrayOf(1, 1, 1, 2, 5, 6, 5), input.nextRoom)
+    }
+
+    @Test
+    fun testGetAnswerSample1() {
+        val input = Input(5,
+                intArrayOf(1, 2, 3, 2, 10),
+                intArrayOf(0, 2, 3, 2, 2))
+        assertEquals(3, Solution(input).getAnswer())
+    }
+
+    @Test
+    fun testGetAnswerSample2() {
+        val input = Input(4,
+                intArrayOf(1, 10, 2, 10),
+                intArrayOf(1, 3, 1, 1))
+        assertEquals(10, Solution(input).getAnswer())
+    }
+
+    @Test
+    fun testGetAnswerSample3() {
+        val input = Input(7,
+                intArrayOf(1, 1, 1, 1, 1, 1, 1),
+                intArrayOf(1, 1, 1, 2, 5, 6, 5))
+        assertEquals(2, Solution(input).getAnswer())
+    }
+
+    @Test(expected = Solution.GetAnswerCalledAgainException::class)
+    fun testGetAnswerCalledTwice() {
+        val input = Input(1,
+                intArrayOf(1),
+                intArrayOf(0))
+        val solution = Solution(input)
+        assertEquals(1, solution.getAnswer())
+        solution.getAnswer()
     }
 }
